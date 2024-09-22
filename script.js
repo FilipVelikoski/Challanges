cf033f48e106dcff8def88e1e4e14ebc165ad0c3
Filip Velikoski <filipvelikoski9@gmail.com>
2024-09-22 16:44:24 +0200

    Added starter files for challange 15

---
+++ script.js
@@ -0,0 +1,36 @@
+const weatherData = [
+  { city: "New York", temperature: 16, humidity: 70, windSpeed: 7 },
+  { city: "London", temperature: 12, humidity: 80, windSpeed: 5 },
+  { city: "Tokyo", temperature: 22, humidity: 60, windSpeed: 4 },
+  { city: "Sydney", temperature: 25, humidity: 50, windSpeed: 6 },
+  { city: "Paris", temperature: 15, humidity: 65, windSpeed: 5 },
+  { city: "Berlin", temperature: 14, humidity: 60, windSpeed: 6 },
+  { city: "Moscow", temperature: 5, humidity: 75, windSpeed: 10 },
+  { city: "Toronto", temperature: 17, humidity: 55, windSpeed: 8 },
+  { city: "Rio de Janeiro", temperature: 26, humidity: 85, windSpeed: 7 },
+  { city: "Beijing", temperature: 20, humidity: 40, windSpeed: 3 },
+  { city: "Mumbai", temperature: 30, humidity: 70, windSpeed: 5 },
+  { city: "Los Angeles", temperature: 19, humidity: 65, windSpeed: 4 },
+  { city: "Cape Town", temperature: 18, humidity: 60, windSpeed: 6 },
+  { city: "Rome", temperature: 21, humidity: 55, windSpeed: 3 },
+  { city: "Bangkok", temperature: 33, humidity: 75, windSpeed: 2 },
+  { city: "Istanbul", temperature: 20, humidity: 60, windSpeed: 4 },
+  { city: "Lagos", temperature: 29, humidity: 80, windSpeed: 3 },
+  { city: "Buenos Aires", temperature: 23, humidity: 70, windSpeed: 5 },
+  { city: "Chicago", temperature: 10, humidity: 65, windSpeed: 7 },
+  { city: "Shanghai", temperature: 19, humidity: 80, windSpeed: 6 },
+];
+
+function fetchWeather(city) {}
+
+function displayCurrentWeather(data) {}
+
+function fetchForecast(city) {}
+
+function displayForecast(data) {}
+
+function searchWeather() {}
+
+// Exercise 03: Save recent searches to local storage
+function saveRecentSearch(city) {}
+function displayRecentSearches() {}
